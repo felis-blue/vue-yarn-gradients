585e07b013ca6ef6b2189039384a8c8b0485d0eb
felis <felis.blue@gmail.com>
2025-03-21 22:14:56 +0100

    change prettier settings

--- src/components/InfoBox.vue
+++ src/components/InfoBox.vue
@@ -4,18 +4,9 @@
     <ul>
       <li>Enter the number of segments and select the number of threads to use.</li>
       <li>The color options and names are based on the TVU Ocean yarn.</li>
-      <li>
-        The yarn is split into segments, each row of segments in the colorway corresponds to one
-        thread.
-      </li>
-      <li>
-        Select and color one or multiple segments by clicking on them, then clicking on the desired
-        color.
-      </li>
-      <li>
-        Shift-click a segment to select all segments of the same color in that row up to the
-        selected segment.
-      </li>
+      <li>The yarn is split into segments, each row of segments in the colorway corresponds to one thread.</li>
+      <li>Select and color one or multiple segments by clicking on them, then clicking on the desired color.</li>
+      <li>Shift-click a segment to select all segments of the same color in that row up to the selected segment.</li>
       <li>Your color selection will automatically be turned into a color swatch.</li>
       <li>Download the swatch as an SVG or PNG via the corresponding buttons.</li>
     </ul>
